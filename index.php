33ba69f3c4ddee43d0c97cf0f111cab035f8adac
Phil <phil@philsmartdesign.com.au>
2014-12-12 16:58:11 +1100

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ * Title
+ *
+ * Description
+ *
+ * @author Phil Smart <phil@philsmartdesign.com.au>
+ * @date: 12/12/14
+ * @copyright 2014 Phil Smart
+ *
+ * @package XXX
+ *
+ */
+
+echo "This is my test, etc.";
